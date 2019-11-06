c6276318b60a478cf81fcd74e73d63e894d58fe1
LUAgam <www.guoao@foxmail.com>
2019-11-06 18:17:37 +0800

    修改部分接口地址

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -2,19 +2,19 @@ const baseUrl = process.env.BASE_API
 const api = {
   state: {
     // 实时控制台
-    socketApi: baseUrl + '/websocket?token=kl',
+    socketApi: baseUrl + 'websocket?token=kl',
     // 图片上传
-    imagesUploadApi: baseUrl + '/api/pictures',
+    imagesUploadApi: baseUrl + 'api/pictures',
     // 修改头像
-    updateAvatarApi: baseUrl + '/api/users/updateAvatar',
+    updateAvatarApi: baseUrl + 'api/users/updateAvatar',
     // 上传文件到七牛云
-    qiNiuUploadApi: baseUrl + '/api/qiNiuContent',
+    qiNiuUploadApi: baseUrl + 'api/qiNiuContent',
     // Sql 监控
-    sqlApi: baseUrl + '/druid',
+    sqlApi: baseUrl + 'druid',
     // swagger
-    swaggerApi: baseUrl + '/swagger-ui.html',
+    swaggerApi: baseUrl + 'swagger-ui.html',
     // 文件上传
-    fileUploadApi: baseUrl + '/api/localStorage',
+    fileUploadApi: baseUrl + 'api/localStorage',
     // baseUrl，
     baseApi: baseUrl
   }
