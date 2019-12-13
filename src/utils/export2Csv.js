727640ad41eddeb55cef8674bb3bfad298f70f9d
www.guoao@foxmail.com <GUOAO8828338>
2019-12-13 12:24:55 +0800

    导出csv大数据量时无响应

--- src/utils/export2Csv.js
+++ src/utils/export2Csv.js
@@ -4,9 +4,9 @@ export function exportCsv(data, fields, fileName) {
   try {
     const json2csvParser = new Parser({ fields: fields, excelStrings: true })
     const csvData = json2csvParser.parse(data)
-    const csvContent = 'data:text/csv;charset=utf-8,\uFEFF' + csvData
     const link = document.createElement('a')
-    link.href = encodeURI(csvContent)
+    var blob = new Blob(['\ufeff' + csvData], { type: 'text/csv' })
+    link.href = URL.createObjectURL(blob)
     link.download = `${fileName}.csv`
     document.body.appendChild(link) // Required for FF
     link.click()
@@ -15,3 +15,4 @@ export function exportCsv(data, fields, fileName) {
     alert(err)
   }
 }
+
